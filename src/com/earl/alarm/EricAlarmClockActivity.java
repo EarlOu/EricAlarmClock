45a2563fe1e22ac1fbcdf87bf63e229a85e07f7d
Shun-Hsing Ou <occqoo@gmail.com>
2012-04-10 18:42:22 +0800

    initial commit

---
+++ src/com/earl/alarm/EricAlarmClockActivity.java
@@ -0,0 +1,14 @@
+package com.earl.alarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EricAlarmClockActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+    }
+}
